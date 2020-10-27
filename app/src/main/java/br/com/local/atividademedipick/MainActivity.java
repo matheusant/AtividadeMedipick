f73d2e7ef97fa9ec417253a04ec4e939d80630aa
Matheus Santos <matheusantos99@gmail.com>
2020-10-27 13:59:33 -0300

    atividade concluida

--- app/src/main/java/br/com/local/atividademedipick/MainActivity.java
+++ app/src/main/java/br/com/local/atividademedipick/MainActivity.java
@@ -7,16 +7,63 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnEntrar, btnSignup;
+    EditText edtUsu, edtSenha;
+    TextView txtEsqueceu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnEntrar = findViewById(R.id.btnEntrar);
+        edtUsu = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnSignup = findViewById(R.id.btnSignup);
+        txtEsqueceu = findViewById(R.id.txtEsqueceu);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = edtUsu.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("12345")){
+
+                    startActivity(new Intent(getApplicationContext(), MainMenu_Activity.class));
+                    finish();
+                }else{
+
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou Senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        txtEsqueceu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
--- app/src/main/java/br/com/local/atividademedipick/MainMenu_Activity.java
+++ app/src/main/java/br/com/local/atividademedipick/MainMenu_Activity.java
@@ -5,19 +5,153 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainMenu_Activity extends AppCompatActivity {
 
-   //ImageButton imbOrder, imbPresc, imbOrders, imbMedic, imbProfile, imbChat, imbPill;
-    //Button btnOrder, btnPresc, btnOrders, btnMedic, btnProfile, btnChat, btnPill;
+    ImageButton imbOrder, imbPresc, imbOrders, imbMedic, imbProfile, imbChat, imbPill;
+    Button btnOrder, btnPresc, btnOrders, btnMedic, btnProfile, btnChat, btnPill;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_layout);
+
+        imbOrder = findViewById(R.id.imbOrder);
+        imbPresc = findViewById(R.id.imbPresc);
+        imbOrders = findViewById(R.id.imbOrders);
+        imbMedic = findViewById(R.id.imbMedic);
+        imbProfile = findViewById(R.id.imbProfile);
+        imbChat = findViewById(R.id.imbChat);
+        imbPill = findViewById(R.id.imbPill);
+
+        btnOrder = findViewById(R.id.btnOrder);
+        btnPresc = findViewById(R.id.btnPresc);
+        btnOrders = findViewById(R.id.btnOrders);
+        btnMedic = findViewById(R.id.btnMedic);
+        btnProfile = findViewById(R.id.btnProfile);
+        btnChat = findViewById(R.id.btnChat);
+        btnPill = findViewById(R.id.btnPill);
+
+        imbOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbPresc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbMedic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        imbPill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnPresc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnMedic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnPill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Em Construção!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     @Override
